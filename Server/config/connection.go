4f03c30855ac833a011ca7e08e5465bcff3dbe56
Sasha <samsiki50@gmail.com>
2024-12-27 03:59:42 +0300

    Add database_test

--- Server/config/connection.go
+++ Server/config/connection.go
@@ -36,6 +36,27 @@ func LoadConfig() *Config {
 	}
 }
 
+func LoadConfigWithPath(envPath string) *Config {
+	if envPath == "" {
+		envPath = ".env"
+	}
+
+	if err := godotenv.Load(envPath); err != nil {
+		log.Fatalf("Ошибка загрузки файла %s: %v", envPath, err)
+	}
+
+	return &Config{
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+		DBSSLMode:  os.Getenv("DB_SSLMODE"),
+		Protocol:   os.Getenv("PROTOCOL"),
+		DBPort:     os.Getenv("DB_PORT"),
+		LPort:      os.Getenv("LPORT"),
+		DBHost:     os.Getenv("DB_HOST"),
+	}
+}
+
 func (c *Config) BuildConnStr() string {
 	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", c.DBHost, c.DBPort, c.DBUser, c.DBPassword, c.DBName, c.DBSSLMode)
 }
---
+++ Server/internal/database/database_test.go
@@ -0,0 +1,194 @@
+package database
+
+import (
+	"Server/config"
+	"database/sql"
+	"fmt"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/lib/pq"
+)
+
+type User struct {
+	name     string
+	password string
+}
+
+func initDB() *sql.DB {
+	cfg := config.LoadConfigWithPath("../../.env")
+	connStr := cfg.BuildConnStr()
+	var err error
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Ошибка подключения к базе данных: %v", err)
+	}
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Ошибка проверки подключения: %s", err)
+	}
+	return db
+}
+
+func TestPostgreSqlConnection(t *testing.T) {
+	var version string
+
+	db := initDB()
+	defer db.Close()
+
+	err := db.Ping()
+	if err != nil {
+		t.Fatalf("Ошибка подключения:%s", err)
+	}
+
+	err = db.QueryRow("SELECT version()").Scan(&version)
+	if err != nil {
+		t.Fatalf("Ошибка получения данных:%s", err)
+	}
+	fmt.Printf("Версия PostgreSQL: %s", version)
+}
+
+func TestShowAllUsersData(t *testing.T) {
+	db := initDB()
+	defer db.Close()
+
+	rows, err := db.Query(`	SELECT users.id, users.name, activity.date 
+    FROM users 
+    JOIN activity ON users.id = activity.idUser`)
+	if err != nil {
+		log.Fatalf("Ошибка вывода данных %s", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var (
+			id   int
+			name string
+			date time.Time
+		)
+		if err := rows.Scan(&id, &name, &date); err != nil {
+			log.Fatalf("Ошибка при чтении строки: %v", err)
+		}
+		formattedDate := date.Format("15:04 02.01.2006")
+		fmt.Printf("|ID: %d\t|Имя: %s\t|Дата: %s|\n", id, name, formattedDate)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Fatalf("Ошибка в процессе итерации: %v", err)
+	}
+}
+
+func TestRegUser(t *testing.T) {
+	var idUser int
+	user := User{"testName", "testPassword"}
+
+	db := initDB()
+	defer db.Close()
+
+	_, err := db.Exec("INSERT INTO users (name, password) VALUES ($1, $2)", user.name, user.password)
+	if err != nil {
+		t.Fatalf("Ошибка добавления пользователя:%s", err)
+	}
+	fmt.Printf("Id добавленного пользователя в системе %d\n", idUser)
+
+	err = db.QueryRow("SELECT id FROM users WHERE name = $1 AND password = $2", user.name, user.password).Scan(&idUser)
+	if err != nil {
+		t.Fatalf("Ошибка проверка пользователя:%s", err)
+	}
+	fmt.Printf("Пользователь найден id: %d name: %s password: %s\n", idUser, user.name, user.password)
+
+	_, err = db.Exec("DELETE FROM users WHERE id = $1", idUser)
+	if err != nil {
+		t.Fatalf("Ошибка удаления пользователя:%s", err)
+	}
+	fmt.Printf("Добавленный пользователь удален\n")
+}
+
+func TestAuthUser(t *testing.T) {
+	var idUser int
+	user := User{"Саша", "123456"}
+
+	db := initDB()
+	defer db.Close()
+
+	err := db.QueryRow("SELECT id FROM users WHERE name = $1 AND password = $2", user.name, user.password).Scan(&idUser)
+	if err != nil {
+		t.Fatalf("Ошибка запроса:%s", err)
+	}
+	log.Printf("Id зарегистрированного пользователя в системе %d", idUser)
+}
+
+func TestInsertTransactions(t *testing.T) {
+	var userId int
+	user := User{"testName", "testPassword"}
+
+	db := initDB()
+	defer db.Close()
+
+	tr, err := db.Begin()
+	if err != nil {
+		t.Fatalf("Ошибка транзакции:%s", err)
+	}
+
+	err = tr.QueryRow("INSERT INTO users (name, password) VALUES ($1, $2) RETURNING id", user.name, user.password).Scan(&userId)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok && pqErr.Code == "23505" {
+			log.Printf("Данный ник уже занят: %v\n", err)
+			_ = tr.Rollback()
+			t.Fatalf("Данный пользователь уже занят:%s", err)
+
+		}
+		_ = tr.Rollback()
+		t.Fatalf("Ошибка добавления пользователя :%s", err)
+
+	}
+
+	location, err := time.LoadLocation("Europe/Moscow")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = tr.Exec("INSERT INTO activity (idUser,date) VALUES ((SELECT id FROM users WHERE name = $1),$2)", user.name, time.Now().In(location))
+	if err != nil {
+		_ = tr.Rollback()
+		t.Fatalf("Ошибка добавлению записи в таблицу activity:%s", err)
+	}
+
+	err = tr.Commit()
+	if err != nil {
+		t.Fatalf("Ошибка выполнения транзакции:%s", err)
+
+	}
+
+	log.Printf("Добавлен новый пользователь: id: %d, name: %s, password: %s\n", userId, user.name, user.password)
+}
+
+func TestDeleteTransactions(t *testing.T) {
+	user := User{"testName", "testPassword"}
+
+	db := initDB()
+	defer db.Close()
+
+	tr, err := db.Begin()
+	if err != nil {
+		t.Fatalf("Ошибка транзакции:%s", err)
+	}
+
+	_, err = tr.Exec("DELETE FROM activity WHERE idUser = (SELECT id FROM users WHERE name = $1 AND password = $2)", user.name, user.password)
+	if err != nil {
+		_ = tr.Rollback()
+		t.Fatalf("Ошибка удаления таблицы activity:%s", err)
+	}
+
+	_, err = tr.Exec("DELETE FROM users WHERE name = $1 AND password = $2", user.name, user.password)
+	if err != nil {
+		_ = tr.Rollback()
+		t.Fatalf("Ошибка удаления таблицы users:%s", err)
+	}
+
+	err = tr.Commit()
+	if err != nil {
+		t.Fatalf("Ошибка выполнения транзакции:%s", err)
+	}
+
+	log.Printf("Пользователь %s %s был удален из таблиц", user.name, user.password)
+}
